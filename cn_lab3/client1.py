e6035a4c2c9b31abd3d0d7a37575209e85ffc47f
lokeshwar <blokeshwar79@gmail.com>
2023-08-16 15:07:05 +0530

    Initial commit

---
+++ cn_lab3/client1.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+
+def receive_messages():
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            print(message)
+        except:
+            print("Error receiving message.")
+            break
+
+def send_messages():
+    while True:
+        recipient_id = input("Enter recipient's unique ID: ")
+        message = input("Enter your message: ")
+        full_message = f"{recipient_id}:{message}"
+        client_socket.send(full_message.encode('utf-8'))
+
+HOST = 'localhost'
+PORT = 12345
+
+client_id = input("Enter your unique ID: ")
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+client_socket.send(client_id.encode('utf-8'))
+
+recv_thread = threading.Thread(target=receive_messages)
+recv_thread.start()
+
+send_thread = threading.Thread(target=send_messages)
+send_thread.start()
---
+++ cn_lab3/server.py
@@ -0,0 +1,48 @@
+import socket
+import threading
+
+clients = {}
+lock = threading.Lock()
+
+def handle_client(client_socket, client_id):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            if not message:
+                break
+            recipient_id, content = message.split(':', 1)
+            send_message(client_id, recipient_id, content)
+        except:
+            break
+
+    with lock:
+        del clients[client_id]
+        print(f"Client {client_id} disconnected")
+        client_socket.close()
+
+def send_message(sender_id, recipient_id, content):
+    with lock:
+        if recipient_id in clients:
+            recipient_socket = clients[recipient_id]
+            try:
+                recipient_socket.send(f"From {sender_id}: {content}".encode('utf-8'))
+            except:
+                pass
+
+HOST = '0.0.0.0'
+PORT = 12345
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+print("Server listening on", HOST, PORT)
+
+while True:
+    client_socket, client_addr = server.accept()
+    with lock:
+        client_id = client_socket.recv(1024).decode('utf-8')
+        clients[client_id] = client_socket
+        print(f"Client {client_id} connected from", client_addr)
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_id))
+        client_thread.start()
